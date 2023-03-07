b13371604bed5a432e9e4d3682ced2378fe75754
Assel Shingissova <asselshin@gmail.com>
2023-03-07 13:27:42 +0600

    1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const imageLi = galleryItems
+  .map(image => {
+    const { preview, original, description } = image;
+    return `<li> <a class="gallery__item" href="${original}"> <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}"/></a></li>`;
+  })
+  .join(' ');
+
+gallery.insertAdjacentHTML('afterbegin', imageLi);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
